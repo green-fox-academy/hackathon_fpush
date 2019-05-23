098a5a26c78af1287bc26f9e1d4f7ef738ff4275
Boritse <szakacs.boro@gmail.com>
2019-05-23 17:42:00 +0200

    sql password face_reco

--- face/face/face.py
+++ face/face/face.py
@@ -1,13 +1,90 @@
 import numpy as np
 import cv2 as cv
 import face_recognition as face
+import sqlite3, random, getpass
+from sqlite3 import Error
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def create_user(conn, user):
+    sql = ''' INSERT INTO legal_people(login_name,password,name,face_id)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, user)
+    return cur.lastrowid
+
+def select_image_by_id(conn, user_name):
+    cur = conn.cursor()
+    cur.execute("SELECT password, name, face_id FROM legal_people WHERE login_name=?", (user_name,))
+    identification_data = cur.fetchall()
+    return identification_data
+ 
+
+database = "C:/GREENFOX/gfa_hack_fpush/database/authentication.db"
+ 
+sql_create_table = """ CREATE TABLE IF NOT EXISTS legal_people (
+                                row_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                login_name TEXT NOT NULL,
+                                password INT NOT NULL,
+                                name TEXT NOT NULL,
+                                face_id TEXT NOT NULL
+                            ); """
+  
+user1 = ('Gyuri', random.randint(100000, 999999), 'Gyorgy Kardos', 'C:/GREENFOX/megalotis-garnet/img/readme_imgs/gy_kardos.jpg')
+user2 = ('Adam', random.randint(100000, 999999), 'Adam Kudar', 'C:/GREENFOX/megalotis-garnet/img/readme_imgs/a_kudar.jpg')
+user3 = ('Snocc', random.randint(100000, 999999), 'Istvan Schneider', 'C:/GREENFOX/megalotis-garnet/img/readme_imgs/i_schneider.jpg')
+user4 = ('Boro', random.randint(100000, 999999), 'Borbala Szakacs', 'C:/GREENFOX/megalotis-garnet/img/readme_imgs/b_szakacs.jpg')
+
+conn = create_connection(database)
+if conn is not None:
+    create_table(conn, sql_create_table)
+else:
+    print("Error! cannot create the database connection.")
+
+#conn = create_connection(database)
+#with conn:
+    #create_user(conn, user1)
+    #create_user(conn, user2)
+    #create_user(conn, user3)
+    #create_user(conn, user4)
+
+username = None
+password = None
+sqlUsername = None
+sqlPassword = None
+
+while(True):
+    username = input("Username:")
+    password = getpass.getpass("Password:")
+    conn = create_connection(database)
+    with conn:
+        user = select_image_by_id(conn, username)
+    if user[0][0] == int(password):
+        break
+
+user_full_name = user[0][1]
+user_image = user[0][2]
 
 cap = cv.VideoCapture(0)
 
 female_image = face.load_image_file("C:/GREENFOX/megalotis-garnet/img/readme_imgs/b_szakacs.jpg")
 female_face_encoding = face.face_encodings(female_image)[0]
 
-male_image = face.load_image_file("C:/GREENFOX/megalotis-garnet/img/readme_imgs/a_kudar.jpg")
+male_image = face.load_image_file(user_image)
 male_face_encoding = face.face_encodings(male_image)[0]
 
 male1_image = face.load_image_file("C:/GREENFOX/megalotis-garnet/img/readme_imgs/gy_kardos.jpg")
@@ -33,7 +110,7 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    frame_number = 1
+    frame_number += 1
     rgb_frame = (frame[:, :, ::-1]).copy()
 
     # Display the resulting frame
